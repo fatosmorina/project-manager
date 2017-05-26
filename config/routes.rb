58489f7713d2416da9416201acb2ffb17237b527
Fatos Morina <fatosi.morina@gmail.com>
2017-05-27 00:22:44 +0200

    Limit comment routes to only create new comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks do
-    resources :comments
+    resources :comments, only: [:create]
   end
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
