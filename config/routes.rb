58bd135a9a213020a0c715fb2799fe6424e77c58
Fatos Morina <fatosi.morina@gmail.com>
2017-05-27 00:21:44 +0200

    Add comment path as a nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    resources :comments
+  end
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
