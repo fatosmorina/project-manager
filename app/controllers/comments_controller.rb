2b2c8d6f4292e17149c6baf5cb67799a2218ec22
Fatos Morina <fatosi.morina@gmail.com>
2017-05-27 01:07:16 +0200

    Implement comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
 class CommentController < ApplicationController
-  
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(params(comment_params))
+
+
+    if @comment.save
+      redirect_to @task, notice: "Comment posted."
+    else
+      redirect_to @task, alert: "Comment not saved" 
+    end
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:name, :email, :body)
+  end
 end
