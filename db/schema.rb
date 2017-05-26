c7d091b288d4cdbf0b5c2a82d118ca8db84ebc88
Fatos Morina <fatosi.morina@gmail.com>
2017-05-26 12:47:29 +0200

    Add project list at task

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525180142) do
+ActiveRecord::Schema.define(version: 20170525190338) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.date "ends_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "done"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
 end
