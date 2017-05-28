4e227b40a0072a7ddee398568f8171d0c40842a0
Fatos Morina <fatosi.morina@gmail.com>
2017-05-28 16:30:20 +0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+ 
+  root "projects#index"
+  
   resources :tasks do
     resources :comments, only: [:create]
   end
-  resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   resources :projects
+   
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
