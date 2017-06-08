3acd936f8aba9f34a38bbf74cb308d5fc7bab732
Fatos Morina <fatosi.morina@gmail.com>
2017-06-08 12:41:33 +0200

    Update change on done javascript function

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,9 @@
-$(function(){
-  $(".task_done").on("change", function(){
-    $.post("/tasks/" + $(this).data("id") + "/toggle", {"_method": "_patch"}, function(){
-    }
-    
-    );
+$(function() {
+  $(".task_done").on("change", function() {
+    var _this = $(this);
+    $.post("/tasks/" + $(this).data("id") + "/toggle", { "_method": "patch" }, function() {
+      _this.parent().effect("highlight");
+    });
   });
 });
+
