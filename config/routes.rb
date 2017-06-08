60036fd9bb44d930f9be941c7095b2e0e4eaafad
Fatos Morina <fatosi.morina@gmail.com>
2017-06-08 12:26:55 +0200

    Add member toggle

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   
   resources :tasks do
     resources :comments, only: [:create]
-  end
+    member do
+      patch :toggle 
+    end
+    
+   end
    resources :projects
    
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
